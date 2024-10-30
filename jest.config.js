ce7853507801be5271ba3d027992a4239c01ef2b
immersir <129412569+immerSIR@users.noreply.github.com>
2024-10-30 17:12:48 +0000

    Update test env and deps

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,10 @@
 module.exports = {
     transform: {
-        "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.(js|jsx)$": "babel-jest",
     },
-    moduleFileExtensions: ["js", "jsx"],
     testEnvironment: "jsdom",
-    setupFiles: ["./jest.setup.js"],
-    setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
     moduleNameMapper: {
         "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-        "^config$": "<rootDir>/src/__mocks__/config.js",
     },
-    moduleDirectories: ["node_modules", "src"],
 };
